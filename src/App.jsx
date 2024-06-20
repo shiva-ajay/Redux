55c98c002c83908846972c623414ca78ed069b47
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-20 23:30:56 +0530

    Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Product from './components/Product'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Ensure Bootstrap CSS is included
 
 const Product = () => {
   const [products, getProducts] = useState([]);
@@ -7,13 +10,34 @@ const Product = () => {
     // api
     fetch('https://fakestoreapi.com/products')
       .then(data => data.json())
-      .then(result => getProducts(result))
+      .then(result => getProducts(result));
   }, []);
+
+  const cards = products.map(product => (
+    <div className="col-md-3" style={{ marginBottom: '10px' }} key={product.id}>
+      <Card className="h-100">
+        <div className="text-center">
+          <Card.Img variant="top" src={product.image} style={{ width: '100px', height: '130px' }} />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>
+            INR: {product.price}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: 'white' }}>
+          <Button variant="primary">Add To Cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
   
   return (
     <>
       <h1>Product Dashboard</h1>
-      {JSON.stringify(products)}
+      <div className='row'>
+        {cards}
+      </div>
     </>
   );
 };
