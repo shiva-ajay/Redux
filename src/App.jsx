7aaf427cd22f0071fdfb6df400427dc944b88b1b
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-21 19:59:32 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Product from './components/Product'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Cart from './components/Cart';
+import RootLayout from './components/RootLayout';
 
-const App = () => {
+
+function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path="/cart" element={<Cart />} />
+    </Route>
+  ));
+  
   return (
-    <>
-    <Product />
-    </>
-  )
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/RootLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import NavBarPanel from './NavBarPanel'
+
+const RootLayout = () => {
+  return (
+    <>
+    <NavBarPanel />
+
+    <main>
+        <Outlet />
+
+    </main>
+    </>
+  )
+}
+
+export default RootLayout
