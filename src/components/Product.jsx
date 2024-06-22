65a543e9f1af13228a9bf3ab2755503d48799a56
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-22 14:58:08 +0530

    states

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,10 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useDispatch, useSelector} from "react-redux";
 import { add } from "../store/cartSlice";
 import { getProducts } from "../store/productSlice";
+import  Alert  from "react-bootstrap/Alert";
 
 const Product = () => {
   const dispatch = useDispatch();
-  const {data: products} = useSelector(state => state.products);
+  const {data: products,status} = useSelector(state => state.products);
 
  
 
@@ -19,6 +20,16 @@ const Product = () => {
     //   .then((data) => data.json())
     //   .then((result) => getProducts(result));
   }, []);
+
+  if(status === 'loading'){
+    return <h2>Loading....</h2>
+  }
+
+  if(status === 'error'){
+    return <Alert key="danger" variant="danger">
+                 Something went Wrong ! Try again Later
+            </Alert>
+  }
 
   const addToCart = (product) => {
     dispatch(add(product));
